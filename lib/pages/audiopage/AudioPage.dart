be98b87854bb20ba2610f525eda50fef97599792
Flajt <matrimkurz@gmail.com>
2022-07-30 17:35:42 +0200

    Add: Folder structure, AudioPage & audio recording plugin + setup

---
+++ lib/pages/audiopage/AudioPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AudioPage extends StatelessWidget {
+  const AudioPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
