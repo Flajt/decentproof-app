ea5197064851a4256a69672ce4f147f6e1534fd0
Flajt <matrimkurz@gmail.com>
2022-08-02 19:19:31 +0200

    Refactor: Remove != false in hashBytesInChunks as it's not needed

--- lib/shared/HashLogic.dart
+++ lib/shared/HashLogic.dart
@@ -11,7 +11,7 @@ class HashLogic {
 
   Future<String> hashBytesInChunks(Uint8List bytes) async {
     final Iterator<int> iterator = bytes.iterator;
-    while (iterator.moveNext() != false) {
+    while (iterator.moveNext()) {
       _keccakDigest.updateByte(iterator.current);
     }
     final Uint8List hashAsBytes = Uint8List(bytes.length);
