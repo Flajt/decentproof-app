8c85064cef2de3e028f7169df9c56601071969a0
Flajt <matrimkurz@gmail.com>
2022-08-02 17:05:48 +0200

    Notice: Add notice to addExifToImage for lter implementation

--- lib/shared/ExifWrapper.dart
+++ lib/shared/ExifWrapper.dart
@@ -1,6 +1,7 @@
 import 'package:native_exif/native_exif.dart';
 
 class ExifWrapper {
+  ///Currently only for testing, method is not used. Will later be used to add location data to images.
   Future<void> addExifToImage(String path) async {
     final Exif exifImageAccess = await Exif.fromPath(path);
     final attributes = await exifImageAccess.getAttributes();
