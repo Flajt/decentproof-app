84082157feca9d14822e8c29457a6dc7a5ad5337
Flajt <matrimkurz@gmail.com>
2022-08-08 14:05:07 +0200

    Update: Add VerificationPage skeletion + added logic to navigate there

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:decentproof/pages/audiopage/AudioPage.dart';
 import 'package:decentproof/pages/homepage/HomePage.dart';
 import 'package:decentproof/pages/submissionpage/SubmissionPage.dart';
 import 'package:decentproof/pages/submissionpage/logic/ShowInExplorer.dart';
+import 'package:decentproof/pages/verificationpage/VerificationPage.dart';
 import 'package:decentproof/pages/videoimagepage/VideoImagePage.dart';
 import 'package:flutter/material.dart';
 
@@ -19,7 +20,8 @@ class MyApp extends StatelessWidget {
         "/videoImagePage": (context) => const VideoImagePage(),
         "/audioPage": (context) => const AudioPage(),
         "/submissionPage": (context) =>
-            SubmissionPage(showInExplorer: ShowInExplorer())
+            SubmissionPage(showInExplorer: ShowInExplorer()),
+        "/verificationPage": (context) => const VerificationPage()
       },
       title: 'Decentproof',
       theme: ThemeData(
--- lib/pages/verificationpage/VerificationPage.dart
+++ lib/pages/verificationpage/VerificationPage.dart
@@ -1,1 +1,24 @@
+import 'package:decentproof/pages/verificationpage/logic/VerificationLogic.dart';
+import 'package:flutter/material.dart';
 
+class VerificationPage extends StatelessWidget {
+  const VerificationPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final VerificationLogic verificationLogic = VerificationLogic();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+          child: SizedBox.fromSize(
+              size: size,
+              child: Stack(children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: OutlinedButton(
+                      onPressed: () {}, child: const Text("Select File")),
+                )
+              ]))),
+    );
+  }
+}
