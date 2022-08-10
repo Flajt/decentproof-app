a8fd91880b9e46b01b9692d08cc117e988b7de00
Flajt <matrimkurz@gmail.com>
2022-08-10 14:44:43 +0200

    Fix breaking: Add required keys to Info.plst to allow app files to be browsed & changed to Filetype.custom to allow selection

--- lib/pages/verificationpage/logic/FileSelectionLogic.dart
+++ lib/pages/verificationpage/logic/FileSelectionLogic.dart
@@ -5,6 +5,8 @@ import 'package:file_picker/file_picker.dart';
 class FileSelectionLogic {
   Future<Stream<List<int>>?> selectFileAsStream() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles(
+        dialogTitle: "Select file to verify",
+        type: FileType.custom,
         withReadStream: true,
         allowCompression: false,
         allowedExtensions: ["png", "aac", "mp4"]);
