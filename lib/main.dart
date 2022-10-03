1669a84e480ae24d3fd80a6fbb9068fd4f98a5f7
Flajt <matrimkurz@gmail.com>
2022-10-03 16:06:05 +0200

    Feat: Initialise AppCheck

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,19 @@ import 'package:decentproof/pages/audiopage/AudioPage.dart';
 import 'package:decentproof/pages/homepage/HomePage.dart';
 import 'package:decentproof/pages/settingspage/SettingsPage.dart';
 import 'package:decentproof/pages/submissionpage/SubmissionPage.dart';
-import 'package:decentproof/pages/submissionpage/logic/ShowInExplorer.dart';
 import 'package:decentproof/pages/verificationpage/VerificationPage.dart';
 import 'package:decentproof/pages/videoimagepage/VideoImagePage.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   //used ONLY for appcheck and nothing more
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseAppCheck.instance.activate();
   runApp(const MyApp());
 }
 
