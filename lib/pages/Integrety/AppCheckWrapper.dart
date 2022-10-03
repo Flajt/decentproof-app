f6e72e60f6763c675971192bbd5b294c77ab8bd4
Flajt <matrimkurz@gmail.com>
2022-10-03 17:07:48 +0200

    Fix: Change getAppToken from static method to regular method

--- lib/pages/Integrety/AppCheckWrapper.dart
+++ lib/pages/Integrety/AppCheckWrapper.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_app_check/firebase_app_check.dart';
 
 class AppcheckWrapper {
-  static Future<String> getAppToken() async {
+  Future<String> getAppToken() async {
     String? appcheckToken = await FirebaseAppCheck.instance.getToken();
     if (appcheckToken == null) throw "No App Check token found!";
     return appcheckToken;
