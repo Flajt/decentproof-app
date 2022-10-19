3c21efeacf410bd9a270e287e8a420f3bbf9b597
Flajt <matrimkurz@gmail.com>
2022-10-19 14:37:09 +0200

    Feat: Crate IntoPage

--- lib/pages/intropage/IntroPage.dart
+++ lib/pages/intropage/IntroPage.dart
@@ -1,12 +1,25 @@
+import 'package:decentproof/pages/intropage/uiblocks/IntoPages.dart';
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return IntroductionScreen(
+      isTopSafeArea: true,
+      isBottomSafeArea: true,
+      pages: introPages,
+      showDoneButton: true,
+      showSkipButton: true,
+      showNextButton: true,
+      dotsDecorator: DotsDecorator(size: Size.square(6.5)),
+      onSkip: () => Navigator.of(context).popAndPushNamed("/"),
+      onDone: () => Navigator.of(context).popAndPushNamed("/"),
+      skip: Text("Skip"),
+      done: Text("Done"),
+      next: Text("Next"),
     );
   }
 }
---
+++ lib/pages/intropage/uiblocks/IntoPages.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+List<PageViewModel> introPages = [
+  PageViewModel(
+      decoration: const PageDecoration(bodyAlignment: Alignment.center),
+      title: "Welcome to Decentproof",
+      bodyWidget: const Center(
+        child: Icon(
+          Icons.waving_hand_rounded,
+          semanticLabel: "Waving hand",
+          size: 150,
+        ),
+      )),
+  PageViewModel(
+      decoration: const PageDecoration(bodyAlignment: Alignment.center),
+      title: "What can you do?",
+      body:
+          "With this App you can create multimedia files and later proof that this file has not been tampered with!"),
+  PageViewModel(
+      decoration: const PageDecoration(bodyAlignment: Alignment.center),
+      title: "How does it work?",
+      body:
+          "Simply select on of the 2 options in the main menu and start creating your file. Supported are: .mp4, .png & .mp3"),
+  PageViewModel(
+      decoration: const PageDecoration(bodyAlignment: Alignment.center),
+      title: "A note about shareing",
+      body:
+          "You can simply share the file from any App you want, but we recommend you use E-Mail, a Cloud storage or direct export.\n\nIf you use other apps like a chat app you might loose metadata and might not be able to later validate the file!"),
+  PageViewModel(
+      decoration: const PageDecoration(bodyAlignment: Alignment.center),
+      title: "Verifying files",
+      body:
+          "Thats easy, simply select the third entry in the main Menu and select your file. Everything else is self explantory!"),
+  PageViewModel(
+      decoration: const PageDecoration(bodyAlignment: Alignment.center),
+      title: "Finish",
+      body:
+          "That's it, if you want to support the App feel free to check the top right icon out, I would appreciate it!")
+];
