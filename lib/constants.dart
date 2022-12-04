46922b3e864e398155e3235c55409b11d5acd32a
Flajt <matrimkurz@gmail.com>
2022-12-04 16:47:00 +0100

    Fix: Remove .env file from version history, urls will be changed on release

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@
 
 import 'package:dotenv/dotenv.dart';
 
-final env = DotEnv(includePlatformEnvironment: true)..load();
+final env = DotEnv(includePlatformEnvironment: true)..load(["../.env"]);
 final String GET_KEY_URL = env["GET_KEY_URL"] ?? "";
 final String CHECK_KEY_URL = env["CHECK_KEY_URL"] ?? "";
 final String SIGN_URL = env["SIGN_URL"] ?? "";
