d7ce707ed11ed6ff5ead018a9cd39b8e20a1dc53
Flajt <matrimkurz@gmail.com>
2022-12-11 13:44:45 +0100

    Fix+Feat: Extend request wait time to 30s & throw error if no api key is stored

--- lib/pages/submissionpage/logic/MessageSigningService.dart
+++ lib/pages/submissionpage/logic/MessageSigningService.dart
@@ -15,12 +15,15 @@ class MessageSigningService {
     ))
       ..interceptors.add(CertificatePinningInterceptor(allowedSHAFingerprints: [
         "72:13:F7:54:38:CF:61:25:AB:90:09:B4:0A:B9:76:C6:AD:DB:89:15:E5:E4:8B:92:8E:0D:A7:0B:28:6D:DB:BD"
-      ], timeout: 20));
+      ], timeout: 30));
     _secureStorageWrapper = wrapper;
   }
   Future<String> signMessage(String hash, DateTime dateTime) async {
     String message = "Hash:$hash App:DecentProof v:0 DateTime:$dateTime";
     String? apiKey = await _secureStorageWrapper.retriveApiKey();
+    if (apiKey == null) {
+      throw "No API KEY";
+    }
     Response resp = await _dio.post("/",
         data: jsonEncode({"data": message}),
         options: Options(headers: {"Authorization": "basic $apiKey"}));
