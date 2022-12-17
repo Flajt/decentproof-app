daa1849778353c61219e4f52b1aa224a4ca21017
Flajt <matrimkurz@gmail.com>
2022-12-17 15:07:58 +0100

    Feat+Fix: Add link to wiki + add Padding to title

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,3 +5,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 final String GET_KEY_URL = dotenv.env["GET_KEY_URL"] ?? "";
 final String CHECK_KEY_URL = dotenv.env["CHECK_KEY_URL"] ?? "";
 final String SIGN_URL = dotenv.env["SIGN_URL"] ?? "";
+// ignore: constant_identifier_names
+const String WIKI_URL = "https://github.com/Flajt/decentproof-app/wiki/FAQ";
--- lib/pages/settingspage/SettingsPage.dart
+++ lib/pages/settingspage/SettingsPage.dart
@@ -1,6 +1,9 @@
 import 'package:decentproof/pages/settingspage/uiblocks/DevNetSwitch.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:decentproof/constants.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -22,12 +25,15 @@ class SettingsPage extends StatelessWidget {
                       onPressed: () => Navigator.of(context).pop(),
                       icon: const Icon(Icons.arrow_back)),
                 ),
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Text(
-                    "settingsPage.title".tr(),
-                    style: Theme.of(context).textTheme.headline5,
-                    textAlign: TextAlign.center,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      "settingsPage.title".tr(),
+                      style: Theme.of(context).textTheme.headline5,
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                 ),
               ],
@@ -41,6 +47,11 @@ class SettingsPage extends StatelessWidget {
             const Divider(),
             const AboutListTile(
               applicationLegalese: "Copyright 2022, Tjalf Bartel",
+            ),
+            const Divider(),
+            ListTile(
+              title: const Text("FAQ"),
+              onTap: () async => await launchUrlString(WIKI_URL),
             ),
             const Divider()
           ],
