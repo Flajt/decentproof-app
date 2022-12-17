2deebcebad347669f0288b5d6332b0de41b989ed
Flajt <matrimkurz@gmail.com>
2022-12-17 02:49:28 +0100

    Feat: Update ImageManager to save to gallery & in app folder

--- lib/pages/videoimagepage/logic/ImageManger.dart
+++ lib/pages/videoimagepage/logic/ImageManger.dart
@@ -4,6 +4,7 @@ import 'package:decentproof/pages/videoimagepage/logic/ImagePickerWrapper.dart';
 import 'package:decentproof/shared/ExifWrapper.dart';
 import 'package:nanoid/async.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 class ImageManager {
   const ImageManager(
@@ -14,11 +15,18 @@ class ImageManager {
   Future<String> saveImage() async {
     Uint8List imageAsBytes = await imagePickerWrapper.getImageAsBytes();
     String path = Platform.isAndroid
-        ? (await getExternalStorageDirectory())!.path
+        ? (await getExternalStorageDirectories(
+                type: StorageDirectory.pictures))![0]
+            .path
         : (await getApplicationDocumentsDirectory()).path;
+    Directory(path).createSync(recursive: true);
     String imageId = await nanoid(16);
     String fullPath = "$path/$imageId.png";
     File(fullPath).writeAsBytesSync(imageAsBytes);
+
+    if (Platform.isAndroid) {
+      await PhotoManager.editor.saveImage(imageAsBytes, title: "$imageId.png");
+    }
     //exifWrapper.addExifToImage("$path/$imageId.png"); TODO: Decide if it's really required
     return fullPath;
   }
