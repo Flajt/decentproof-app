31cab2625f16f4eeb691857288deb5e4ceaa70ad
Flajt <matrimkurz@gmail.com>
2022-12-17 13:36:43 +0100

    Chore: Removing dead imports

--- lib/pages/intropage/IntroPage.dart
+++ lib/pages/intropage/IntroPage.dart
@@ -1,7 +1,4 @@
-import 'dart:io';
-
 import 'package:decentproof/pages/intropage/uiblocks/IntoPages.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
