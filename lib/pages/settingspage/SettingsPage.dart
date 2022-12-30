271226a53d77c7133bba6308a47e4133acfc0fb2
Flajt <matrimkurz@gmail.com>
2022-12-30 19:04:47 +0100

    Chore: Remove dead import

--- lib/pages/settingspage/SettingsPage.dart
+++ lib/pages/settingspage/SettingsPage.dart
@@ -1,7 +1,6 @@
 import 'package:decentproof/pages/settingspage/uiblocks/DevNetSwitch.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:decentproof/constants.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
