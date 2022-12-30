1d06657ac9e7fd2eeed0d1732959610a94cc75ba
Flajt <matrimkurz@gmail.com>
2022-12-30 19:05:31 +0100

    Fix: Update fingerprint for IOTA devnet to reflect SSL certificate changes

--- lib/shared/interceptors/IotaPinningInterceptor.dart
+++ lib/shared/interceptors/IotaPinningInterceptor.dart
@@ -13,7 +13,7 @@ class IotaSSLPinningInterceptor extends RequestInterceptor {
         timeout: 30,
         allowedSHAFingerprints: [
           "96:67:83:59:C9:AF:6C:73:F6:B7:A9:68:B5:C6:50:85:63:F1:E2:D6:13:E0:57:F0:C2:EA:4B:C6:5F:83:76:68",
-          "21:5F:37:BB:80:7F:FB:04:38:CE:6D:65:E1:C6:9B:A9:8A:39:89:D6:0D:29:75:3E:1C:2A:59:CB:59:62:4D:12"
+          "B3:1C:6E:CB:D6:63:95:65:B8:1E:6A:D8:E7:37:A6:3A:6B:74:76:C0:23:0F:76:34:07:E3:79:9D:F6:F3:FB:B0"
         ]);
     if (secure == "CONNECTION_SECURE") {
       return request;
