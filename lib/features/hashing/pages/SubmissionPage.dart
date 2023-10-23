5c2be5472fd5fdb0943d523decde47b4fa79ed3c
Flajt <matrimkurz@gmail.com>
2023-10-23 14:37:44 +0200

    Feat: Add ErrorDialog + upadte TextSyles

--- lib/features/hashing/pages/SubmissionPage.dart
+++ lib/features/hashing/pages/SubmissionPage.dart
@@ -1,12 +1,11 @@
+import 'package:decentproof/features/hashing/bloc/SubmissionBloc.dart';
+import 'package:decentproof/features/hashing/bloc/SubmissionEvents.dart';
 import 'package:decentproof/features/hashing/bloc/SubmissionState.dart';
 import 'package:decentproof/shared/uiblocks/ErrorDialog.dart';
-import 'package:decentproof/shared/uiblocks/ProcessingDialog.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../logic/backend/HashSubmissionService.dart';
-import '../logic/backend/ShowInExplorer.dart';
 import '../uiblocks/BackToHomeButton.dart';
 import '../uiblocks/ShareButton.dart';
 
@@ -21,59 +20,74 @@ class SubmissionPage extends StatelessWidget {
         body: SafeArea(
             child: SizedBox.fromSize(
       size: size,
-      child: Stack(
-        children: [
-          Align(
-            alignment: Alignment.topRight,
-            child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ShareButton(filePath: args["path"]!)),
-          ),
-          const Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: BackToHomeButton(),
+      child: BlocListener<SubmissionBloc, SubmissionStates>(
+        listener: (context, state) {
+          if (state is SubmissionError) {
+            showDialog(
+                context: context,
+                builder: (context) => ErrorDialog(
+                      error: state.message,
+                      size: size,
+                    ));
+          }
+        },
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ShareButton(filePath: args["path"]!)),
             ),
-          ),
-          Positioned(
-            height: size.height * .2,
-            width: size.width - 10,
-            top: size.height * .2,
-            child: Text(
-              "${"submissionPage.hash".tr()}\n\n ${args["hash"]}",
-              textAlign: TextAlign.center,
-              style:
-                  const TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: BackToHomeButton(),
+              ),
             ),
-          ),
-          BlocBuilder(builder: (context, state) {
-            if (state is SubmissionInitial) {
-              return Align(
+            Positioned(
+              height: size.height * .2,
+              width: size.width - 10,
+              top: size.height * .2,
+              child: Text(
+                "${"submissionPage.hash".tr()}\n\n ${args["hash"]}",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+            ),
+            BlocBuilder(builder: (context, state) {
+              if (state is SubmissionInitial) {
+                return Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                          onPressed: () async {
+                            context
+                                .read<SubmissionBloc>()
+                                .add(SubmitHash(args["hash"]!));
+                          },
+                          child: const Text("submissionPage.submitt").tr()),
+                    ));
+              } else if (state is SubmissionSuccessfull) {
+                return Align(
                   alignment: Alignment.bottomCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        onPressed: () async {},
-                        child: const Text("submissionPage.submitt").tr()),
-                  ));
-            } else if (state is SubmissionSuccessfull) {
-              return Align(
-                alignment: Alignment.bottomCenter,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text("submissionPage.submissionSuccess",
-                            style: Theme.of(context).textTheme.headlineMedium)
-                        .tr(),
-                    const BackToHomeButton()
-                  ],
-                ),
-              );
-            }
-            return const Center(child: CircularProgressIndicator.adaptive());
-          })
-        ],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text("submissionPage.submissionSuccess",
+                              style: Theme.of(context).textTheme.headlineMedium)
+                          .tr(),
+                      const BackToHomeButton()
+                    ],
+                  ),
+                );
+              }
+              return const Center(child: CircularProgressIndicator.adaptive());
+            })
+          ],
+        ),
       ),
     )));
   }
