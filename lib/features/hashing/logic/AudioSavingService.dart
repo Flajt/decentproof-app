ac01e5bd02f59ee1f818d01d9c0a731febd55802
Flajt <matrimkurz@gmail.com>
2023-10-25 13:42:49 +0200

    Fix: Issues with creation of missing folders

--- lib/features/hashing/logic/AudioSavingService.dart
+++ lib/features/hashing/logic/AudioSavingService.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:decentproof/shared/util/PathUtil.dart';
 import 'package:nanoid/async.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -8,8 +9,9 @@ import '../interfaces/IFileSavingService.dart';
 class AudioSavingService implements IFileSavingService {
   @override
   Future<String> saveFile() async {
-    return Platform.isAndroid
-        ? "${(await getExternalStorageDirectories(type: StorageDirectory.podcasts))![0].path}/${await nanoid(16)}.aac"
-        : "${(await getApplicationDocumentsDirectory()).path}/${await nanoid(16)}.aac";
+    Directory dir = await PathUtil.getStoragePath(StorageDirectory.podcasts);
+    await dir.create(recursive: true);
+    String path = dir.path;
+    return "$path/${await nanoid(16)}.aac";
   }
 }
