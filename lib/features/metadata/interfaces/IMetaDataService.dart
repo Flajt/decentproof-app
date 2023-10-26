ab2b365dd78a017545c82c26c5c21b6caf1a045c
Flajt <matrimkurz@gmail.com>
2023-10-26 14:13:46 +0200

    Fix: Add missing file path

--- lib/features/metadata/interfaces/IMetaDataService.dart
+++ lib/features/metadata/interfaces/IMetaDataService.dart
@@ -2,12 +2,12 @@ import 'package:decentproof/features/metadata/models/LocationModel.dart';
 
 abstract class IMetaDataService {
   ///Adds location to metadata
-  Future<String> addLocation(LocationModel locationModel);
+  Future<String> addLocation(LocationModel locationModel, String filePath);
 
   ///Adds secret to file metatdata
-  Future<String> addSecret(String secretHash);
+  Future<String> addSecret(String secretHash, String filePath);
 
   ///Convenience method to add both location and secret
   Future<String> addLocationAndSecret(
-      LocationModel locationModel, String secretHash);
+      LocationModel locationModel, String secretHash, String filePath);
 }
