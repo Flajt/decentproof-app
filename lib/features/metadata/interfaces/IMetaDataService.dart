7b4b7b547c61741fa1afbf9342b1bbe686ba08e9
Flajt <matrimkurz@gmail.com>
2023-10-26 13:53:44 +0200

    Refactor+Feat: Refactor MetaData service into two+impl

--- lib/features/metadata/interfaces/IMetaDataService.dart
+++ lib/features/metadata/interfaces/IMetaDataService.dart
@@ -1,7 +1,13 @@
+import 'package:decentproof/features/metadata/models/LocationModel.dart';
+
 abstract class IMetaDataService {
-  Future<bool> shouldEmbedLocation();
-  Future<bool> shouldEmbedSecret();
-  Future<void> embedLocation(bool should);
-  Future<void> embedSecret(bool should);
-  Future<String> embedLocationASecret();
+  ///Adds location to metadata
+  Future<String> addLocation(LocationModel locationModel);
+
+  ///Adds secret to file metatdata
+  Future<String> addSecret(String secretHash);
+
+  ///Convenience method to add both location and secret
+  Future<String> addLocationAndSecret(
+      LocationModel locationModel, String secretHash);
 }
