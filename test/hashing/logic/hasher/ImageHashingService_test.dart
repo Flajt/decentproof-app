cd71d6676ef27673af892c06c41cc47847dfd5e7
Flajt <matrimkurz@gmail.com>
2023-11-06 16:01:58 +0100

    Feat:Test ImageHashingService&add icon to fixtures

---
+++ test/hashing/logic/hasher/ImageHashingService_test.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:decentproof/features/hashing/logic/hasher/ImageHashingService.dart';
+import 'package:decentproof/shared/HashLogic.dart';
+import 'package:decentproof/shared/interface/IHashLogic.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
+import 'package:test/test.dart';
+
+void main() {
+  final GetIt getIt = GetIt.I;
+  const imageHash =
+      "3edd6754e7ef3164f0814ccf55baf4f619572b88ecf1ab6579515bc6e4393546";
+
+  setUp(() async {
+    await getIt.reset();
+  });
+
+  group("AudioHashingService", () {
+    getIt.registerFactory<IHashLogic>(() => HashLogic());
+    final ImageHashingService service = ImageHashingService();
+
+    group("successfully", () {
+      test("hash audio file", () async {
+        String hash = await service.hash("test/fixtures/icon.png");
+        expect(hash, equals(imageHash));
+      });
+    });
+  });
+}
