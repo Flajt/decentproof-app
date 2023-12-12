6078570677b395660a5671779dda7ca6276c4e76
Flajt <matrimkurz@gmail.com>
2023-12-12 17:25:33 +0100

    Fix: Prevent 2x dialog spawining by moving bloc listener to page not buttons

--- lib/features/hashing/uiblocks/VideoButton.dart
+++ lib/features/hashing/uiblocks/VideoButton.dart
@@ -1,10 +1,5 @@
 import 'package:decentproof/features/hashing/bloc/PreparationBloc/PreparationBloc.dart';
 import 'package:decentproof/features/hashing/bloc/PreparationBloc/PerparationEvents.dart';
-import 'package:decentproof/features/hashing/bloc/PreparationBloc/PerparationStates.dart';
-import 'package:decentproof/features/hashing/bloc/SubmissionBloc.dart';
-import 'package:decentproof/features/hashing/bloc/SubmissionEvents.dart';
-import 'package:decentproof/shared/uiblocks/ErrorDialog.dart';
-import 'package:decentproof/shared/uiblocks/ProcessingDialog.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,49 +9,31 @@ class VideoButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return BlocListener<PreparationBloc, PreparationState>(
-      listener: (context, state) {
-        if (state is PreparationHasError) {
-          Navigator.of(context).pop();
-          showDialog(
-              context: context,
-              builder: (context) =>
-                  ErrorDialog(size: size, error: state.errorMsg));
-        } else if (state is PreparationIsSuccessfull) {
-          context.read<SubmissionBloc>().add(ResetSubmissionState());
-          Navigator.of(context).pushNamed("/submissionPage",
-              arguments: {"hash": state.hash, "path": state.path});
-        } else if (state is PrepareationIsAplyingWaterMark) {
-          showDialog(
-              context: context, builder: (context) => const ProcessingDialog());
-        }
-      },
-      child: Container(
-        width: size.width * .6,
-        height: size.height * .2,
-        decoration: BoxDecoration(
-            border: Border.all(color: Colors.black, width: 2.0),
-            borderRadius: const BorderRadius.all(Radius.circular(20.0))),
-        child: ClipRRect(
-          borderRadius: const BorderRadius.all(Radius.circular(20.0)),
-          child: Material(
-            color: Colors.transparent,
-            child: InkWell(
-              onTap: () => context.read<PreparationBloc>().add(PrepareVideo()),
-              radius: size.width,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const Icon(
-                      Icons.video_file,
-                      size: 100.0,
-                    ),
-                    const Text("videoImagePage.video",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20.0))
-                        .tr()
-                  ]),
-            ),
+    return Container(
+      width: size.width * .6,
+      height: size.height * .2,
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 2.0),
+          borderRadius: const BorderRadius.all(Radius.circular(20.0))),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: () => context.read<PreparationBloc>().add(PrepareVideo()),
+            radius: size.width,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Icon(
+                    Icons.video_file,
+                    size: 100.0,
+                  ),
+                  const Text("videoImagePage.video",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20.0))
+                      .tr()
+                ]),
           ),
         ),
       ),
