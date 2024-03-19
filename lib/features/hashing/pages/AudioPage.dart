0b72f1f26bbebad8d4f44a178e5127cbb5f219fa
Flajt <matrimkurz@gmail.com>
2024-03-19 11:41:19 +0100

    Fix: Rm WithForegroundTask widget

--- lib/features/hashing/pages/AudioPage.dart
+++ lib/features/hashing/pages/AudioPage.dart
@@ -31,40 +31,39 @@ class _AudioPageState extends State<AudioPage> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return WithForegroundTask(
-      child: Scaffold(
-        body: SafeArea(
-            child: SizedBox(
-          width: size.width,
-          height: size.height,
-          child: Stack(children: [
-            Align(
-              child: AudioWaveforms(
-                size: Size(size.width * .8, 150),
-                recorderController: recorderController,
-                waveStyle: WaveStyle(
-                    showDurationLabel: true,
-                    durationLinesColor: Colors.transparent,
-                    waveColor: MediaQuery.of(context).platformBrightness ==
-                            Brightness.light
-                        ? Theme.of(context).primaryColor
-                        : Colors.orange,
-                    extendWaveform: true,
-                    showMiddleLine: false),
+    return Scaffold(
+      //Note to self: WithForegroundTask was here
+      body: SafeArea(
+          child: SizedBox(
+        width: size.width,
+        height: size.height,
+        child: Stack(children: [
+          Align(
+            child: AudioWaveforms(
+              size: Size(size.width * .8, 150),
+              recorderController: recorderController,
+              waveStyle: WaveStyle(
+                  showDurationLabel: true,
+                  durationLinesColor: Colors.transparent,
+                  waveColor: MediaQuery.of(context).platformBrightness ==
+                          Brightness.light
+                      ? Theme.of(context).primaryColor
+                      : Colors.orange,
+                  extendWaveform: true,
+                  showMiddleLine: false),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RecordingButton(
+                controller: recorderController,
               ),
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: RecordingButton(
-                  controller: recorderController,
-                ),
-              ),
-            )
-          ]),
-        )),
-      ),
+          )
+        ]),
+      )),
     );
   }
 }
--- lib/features/verification/pages/VerificationPage.dart
+++ lib/features/verification/pages/VerificationPage.dart
@@ -18,107 +18,103 @@ class VerificationPage extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     final ShowInExplorer showInExplorer = ShowInExplorer();
-    return WithForegroundTask(
-      child: Scaffold(
-        body: SafeArea(
-          child: BlocConsumer<VerificationBloc, VerificationBlocStates>(
-              listener: (context, state) {
-            if (state is ErrorState) {
-              showDialog(
-                  context: context,
-                  builder: (context) => ErrorDialog(
-                        size: size,
-                        error: state.message,
-                      ));
-              context.read<VerificationBloc>().add(ResetEvent());
-            }
-          }, builder: (context, state) {
-            if (state is InitialState) {
-              return Column(
-                children: [
-                  Expanded(
-                      flex: 1,
-                      child: FractionallySizedBox(
-                        heightFactor: .5,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text("verificationPage.title",
-                                  style:
-                                      Theme.of(context).textTheme.headlineLarge)
-                              .tr(),
-                        ),
-                      )),
-                  Expanded(
+    return Scaffold(
+      // Note to self: With Foreground Task was here
+      body: SafeArea(
+        child: BlocConsumer<VerificationBloc, VerificationBlocStates>(
+            listener: (context, state) {
+          if (state is ErrorState) {
+            showDialog(
+                context: context,
+                builder: (context) => ErrorDialog(
+                      size: size,
+                      error: state.message,
+                    ));
+            context.read<VerificationBloc>().add(ResetEvent());
+          }
+        }, builder: (context, state) {
+          if (state is InitialState) {
+            return Column(
+              children: [
+                Expanded(
                     flex: 1,
-                    child: Center(
-                        child: FilledButton(
-                      onPressed: () => context
-                          .read<VerificationBloc>()
-                          .add(VerifyHashEvent()),
-                      child: const Text("verificationPage.selectFile").tr(),
+                    child: FractionallySizedBox(
+                      heightFactor: .5,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("verificationPage.title",
+                                style:
+                                    Theme.of(context).textTheme.headlineLarge)
+                            .tr(),
+                      ),
                     )),
-                  ),
-                ],
-              );
-            } else if (state is VerifiedState) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text("verificationPage.title",
-                          style: Theme.of(context).textTheme.headlineLarge)
-                      .tr(),
-                  CheckMarkTable(statusModel: state.statusModel),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      //TODO: Consider moving this to a separate widget in the near future
-                      state.statusModel.bitcoinTransaction != null
-                          ? FilledButton.icon(
-                              style: const ButtonStyle(
-                                  padding: MaterialStatePropertyAll(
-                                      EdgeInsets.all(10.0)),
-                                  backgroundColor: MaterialStatePropertyAll(
-                                      Color(0xffF7931A))),
-                              icon: const Icon(Icons.currency_bitcoin),
-                              onPressed: () => showInExplorer.show(
-                                  transaction:
-                                      state.statusModel.bitcoinTransaction!),
-                              label:
-                                  const Text("verificationPage.showInExplorer")
-                                      .tr())
-                          : Container(),
-                      state.statusModel.ethereumTransaction != null
-                          ? FilledButton.icon(
-                              style: const ButtonStyle(
-                                  padding: MaterialStatePropertyAll(
-                                      EdgeInsets.all(10.0)),
-                                  backgroundColor: MaterialStatePropertyAll(
-                                      Color(0xff343434))),
-                              icon: const Icon(DecentProof.etherium,
-                                  color: Colors.white),
-                              onPressed: () => showInExplorer.show(
-                                  transaction:
-                                      state.statusModel.ethereumTransaction!,
-                                  network: Network.etherium),
-                              label: const Text(
-                                      "verificationPage.showInExplorer",
-                                      style: TextStyle(color: Colors.white))
-                                  .tr())
-                          : Container(),
-                    ],
-                  ),
-                  OutlinedButton(
+                Expanded(
+                  flex: 1,
+                  child: Center(
+                      child: FilledButton(
                     onPressed: () =>
-                        context.read<VerificationBloc>().add(ResetEvent()),
-                    child: const Text("verificationPage.reset").tr(),
-                  ),
-                ],
-              );
-            }
-            return const Center(child: CircularProgressIndicator.adaptive());
-          }),
-        ),
+                        context.read<VerificationBloc>().add(VerifyHashEvent()),
+                    child: const Text("verificationPage.selectFile").tr(),
+                  )),
+                ),
+              ],
+            );
+          } else if (state is VerifiedState) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text("verificationPage.title",
+                        style: Theme.of(context).textTheme.headlineLarge)
+                    .tr(),
+                CheckMarkTable(statusModel: state.statusModel),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    //TODO: Consider moving this to a separate widget in the near future
+                    state.statusModel.bitcoinTransaction != null
+                        ? FilledButton.icon(
+                            style: const ButtonStyle(
+                                padding: MaterialStatePropertyAll(
+                                    EdgeInsets.all(10.0)),
+                                backgroundColor: MaterialStatePropertyAll(
+                                    Color(0xffF7931A))),
+                            icon: const Icon(Icons.currency_bitcoin),
+                            onPressed: () => showInExplorer.show(
+                                transaction:
+                                    state.statusModel.bitcoinTransaction!),
+                            label: const Text("verificationPage.showInExplorer")
+                                .tr())
+                        : Container(),
+                    state.statusModel.ethereumTransaction != null
+                        ? FilledButton.icon(
+                            style: const ButtonStyle(
+                                padding: MaterialStatePropertyAll(
+                                    EdgeInsets.all(10.0)),
+                                backgroundColor: MaterialStatePropertyAll(
+                                    Color(0xff343434))),
+                            icon: const Icon(DecentProof.etherium,
+                                color: Colors.white),
+                            onPressed: () => showInExplorer.show(
+                                transaction:
+                                    state.statusModel.ethereumTransaction!,
+                                network: Network.etherium),
+                            label: const Text("verificationPage.showInExplorer",
+                                    style: TextStyle(color: Colors.white))
+                                .tr())
+                        : Container(),
+                  ],
+                ),
+                OutlinedButton(
+                  onPressed: () =>
+                      context.read<VerificationBloc>().add(ResetEvent()),
+                  child: const Text("verificationPage.reset").tr(),
+                ),
+              ],
+            );
+          }
+          return const Center(child: CircularProgressIndicator.adaptive());
+        }),
       ),
     );
   }
