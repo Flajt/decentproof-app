589f6f783635563727550dab0b337effaed17021
Flajt <matrimkurz@gmail.com>
2024-07-21 17:16:24 +0200

    Fix: Setup missing hydrated_bloc storage logic

--- lib/main.dart
+++ lib/main.dart
@@ -27,11 +27,15 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   runZonedGuarded(() async {
     WidgetsFlutterBinding.ensureInitialized();
+    HydratedBloc.storage = await HydratedStorage.build(
+        storageDirectory: await getApplicationDocumentsDirectory());
     await EasyLocalization.ensureInitialized();
     await registar();
     await dotenv.load();
