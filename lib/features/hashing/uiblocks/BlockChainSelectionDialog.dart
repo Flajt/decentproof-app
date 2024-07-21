860a4f196d43dd1f13dcaecf9e05bc0f3f5c4e73
Flajt <matrimkurz@gmail.com>
2024-07-21 17:16:51 +0200

    Fix: Fix BlockchainSelectionDialog styling and translation titles

--- lib/features/hashing/uiblocks/BlockChainSelectionDialog.dart
+++ lib/features/hashing/uiblocks/BlockChainSelectionDialog.dart
@@ -1,7 +1,9 @@
 import 'package:decentproof/features/hashing/bloc/BlockChainCubit/BlockChainCubit.dart';
 import 'package:decentproof/features/metadata/enum/BlockChainEnum.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:simple_icons/simple_icons.dart';
 
 class BlockChainSelectionDialog extends StatelessWidget {
   const BlockChainSelectionDialog({super.key});
@@ -9,32 +11,63 @@ class BlockChainSelectionDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
-    final BlockChainCubit blockChainCubit = context.read<BlockChainCubit>();
     return Dialog(
       child: SizedBox(
-        width: size.width * .4,
-        height: size.height * .4,
-        child: Column(
-          children: [
-            Text("Title", style: Theme.of(context).textTheme.titleMedium),
-            Text("", style: Theme.of(context).textTheme.bodyMedium),
-            Radio.adaptive(
-              value: BlockChain.BTC,
-              groupValue: blockChainCubit.state,
-              onChanged: (chain) => blockChainCubit.setBlockChain(chain!),
-            ),
-            Radio.adaptive(
-                value: BlockChain.ETH,
-                groupValue: blockChainCubit.state,
-                onChanged: (chain) => blockChainCubit.setBlockChain(chain!)),
-            ElevatedButton(
-                onPressed: () {
-                  if (blockChainCubit.state != null) {
-                    Navigator.of(context).pop();
-                  }
-                },
-                child: Text(""))
-          ],
+        width: size.width * .45,
+        height: size.height * .45,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: BlocBuilder<BlockChainCubit, BlockChain?>(
+            builder: (context, state) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("homePage.blockChainSelectionDialog.title".tr(),
+                      style: Theme.of(context).textTheme.titleLarge),
+                  Text("homePage.blockChainSelectionDialog.description".tr(),
+                      style: Theme.of(context).textTheme.bodyMedium,
+                      textAlign: TextAlign.center),
+                  RadioListTile.adaptive(
+                      value: BlockChain.BTC,
+                      groupValue: state,
+                      onChanged: (chain) =>
+                          context.read<BlockChainCubit>().setBlockChain(chain!),
+                      title: const SizedBox(
+                        width: 50,
+                        child: Row(
+                          children: [
+                            Icon(Icons.currency_bitcoin),
+                            Text("Bitcoin")
+                          ],
+                        ),
+                      )),
+                  RadioListTile.adaptive(
+                    value: BlockChain.ETH,
+                    groupValue: state,
+                    onChanged: (chain) =>
+                        context.read<BlockChainCubit>().setBlockChain(chain!),
+                    title: const SizedBox(
+                      width: 50,
+                      child: Row(
+                        children: [
+                          Icon(SimpleIcons.ethereum),
+                          Text("Ethereum")
+                        ],
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        if (state != null) {
+                          Navigator.of(context).pop();
+                        }
+                      },
+                      child: Text(
+                          "homePage.blockChainSelectionDialog.submit".tr()))
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
