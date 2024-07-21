b499b82db261ac6d313896e97348184f5c73de65
Flajt <matrimkurz@gmail.com>
2024-07-21 17:43:51 +0200

    Chore: Update SubmissionBloc to use blockChain

--- lib/features/hashing/bloc/SubmissionEvents.dart
+++ lib/features/hashing/bloc/SubmissionEvents.dart
@@ -1,8 +1,11 @@
+import 'package:decentproof/features/metadata/enum/BlockChainEnum.dart';
+
 abstract class SubmissionEvent {}
 
 class SubmitHash extends SubmissionEvent {
   final String hash;
-  SubmitHash(this.hash);
+  final BlockChain blockChain;
+  SubmitHash(this.hash, this.blockChain);
 }
 
 class ResetSubmissionState extends SubmissionEvent {}
