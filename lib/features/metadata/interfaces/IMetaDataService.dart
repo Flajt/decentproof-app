987bea74e5ad9cb8f1a0fefdc5d29de7d7ff8787
Flajt <matrimkurz@gmail.com>
2024-07-21 15:00:33 +0200

    Feat: Upgrade IMetaDataService to include BlockChain

--- lib/features/metadata/interfaces/IMetaDataService.dart
+++ lib/features/metadata/interfaces/IMetaDataService.dart
@@ -1,16 +1,19 @@
+import 'package:decentproof/features/metadata/enum/BlockChainEnum.dart';
 import 'package:decentproof/features/metadata/models/LocationModel.dart';
 import 'package:decentproof/features/metadata/models/MetaDataModel.dart';
 
 abstract class IMetaDataService {
   ///Adds location to metadata
-  Future<String> addLocation(LocationModel locationModel, String filePath);
+  Future<String> addLocation(
+      LocationModel locationModel, String filePath, BlockChain blockChain);
 
   ///Adds secret to file metatdata
-  Future<String> addSecret(String secretHash, String filePath);
+  Future<String> addSecret(
+      String secretHash, String filePath, BlockChain blockChain);
 
   ///Convenience method to add both location and secret
-  Future<String> addLocationAndSecret(
-      LocationModel locationModel, String secretHash, String filePath);
+  Future<String> addLocationAndSecret(LocationModel locationModel,
+      String secretHash, String filePath, BlockChain blockChain);
 
   Future<MetaDataModel> retriveMetaData(String filePath);
 }
