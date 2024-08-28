6cafcd7bc7db5f12b6762ea1fd1835420bbfc07a
Flajt <matrimkurz@gmail.com>
2024-08-28 17:23:53 +0200

    Feat: Upgrade imageMetaDataService to work as intended

--- lib/features/metadata/logic/metaDataServices/ImageMetaDataService.dart
+++ lib/features/metadata/logic/metaDataServices/ImageMetaDataService.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
+import 'package:image/image.dart' as img;
 
 import 'package:decentproof/constants.dart';
 import 'package:decentproof/features/metadata/enum/BlockChainEnum.dart';
 import 'package:decentproof/features/metadata/interfaces/IMetaDataService.dart';
 import 'package:decentproof/features/metadata/models/LocationModel.dart';
 import 'package:decentproof/features/metadata/models/MetaDataModel.dart';
-import 'package:native_exif/native_exif.dart';
 
 class ImageMetaDataService implements IMetaDataService {
   const ImageMetaDataService();
@@ -24,20 +24,15 @@ class ImageMetaDataService implements IMetaDataService {
   @override
   Future<String> addLocation(LocationModel locationModel, String filePath,
       BlockChain blockChain) async {
-    print(filePath);
-    print(_isIOS);
-    print({(_isIOS! ? "FileSource" : "Software"): "Decentproof $DPM_VERSION"});
     if (_isIOS == null) {
       throw "Not supported on this platform";
     }
-    Exif exif = await Exif.fromPath(filePath);
-    await exif.writeAttributes({
-      "GPSLatitude": locationModel.latitude,
-      "GPSLongitude": locationModel.longitude,
-      (_isIOS! ? "FileSource" : "Software"): "Decentproof $DPM_VERSION",
-      "UserComment": blockChain.name
-    });
-    await exif.close();
+    final image = (await img.decodeJpgFile(filePath))!;
+    image.exif.imageIfd.software = "Decentproof $DPM_VERSION";
+    image.exif.imageIfd.userComment = blockChain.name;
+    image.exif.gpsIfd.gpsLatitude = locationModel.latitude;
+    image.exif.gpsIfd.gpsLongitude = locationModel.longitude;
+    await img.encodeJpgFile(filePath, image);
     return filePath;
   }
 
@@ -47,15 +42,13 @@ class ImageMetaDataService implements IMetaDataService {
     if (_isIOS == null) {
       throw "Not supported on this platform";
     }
-    Exif exif = await Exif.fromPath(filePath);
-    await exif.writeAttributes({
-      "GPSLatitude": locationModel.latitude,
-      "GPSLongitude": locationModel.longitude,
-      _isIOS! ? "AuxOwnerName" : "Artist": secretHash,
-      _isIOS! ? "FileSource" : "Software": "Decentproof $DPM_VERSION",
-      "UserComment": blockChain.name
-    });
-    await exif.close();
+    final image = (await img.decodeJpgFile(filePath))!;
+    image.exif.imageIfd.software = "Decentproof $DPM_VERSION";
+    image.exif.imageIfd.userComment = blockChain.name;
+    image.exif.gpsIfd.gpsLatitude = locationModel.latitude;
+    image.exif.gpsIfd.gpsLongitude = locationModel.longitude;
+    //TODO add artist tag with secrets
+    await img.encodeJpgFile(filePath, image);
     return filePath;
   }
 
@@ -65,13 +58,11 @@ class ImageMetaDataService implements IMetaDataService {
     if (_isIOS == null) {
       throw "Not supported on this platform";
     }
-    Exif exif = await Exif.fromPath(filePath);
-    await exif.writeAttributes({
-      _isIOS! ? "AuxOwnerName" : "Artist": secretHash,
-      _isIOS! ? "FileSource" : "Software": "Decentproof $DPM_VERSION",
-      "UserComment": blockChain.name
-    });
-    await exif.close();
+    final image = (await img.decodeJpgFile(filePath))!;
+    image.exif.imageIfd.software = "Decentproof $DPM_VERSION";
+    image.exif.imageIfd.userComment = blockChain.name;
+    //TODO add artist tag with secrets
+    await img.encodeJpgFile(filePath, image);
     return filePath;
   }
 
@@ -84,40 +75,28 @@ class ImageMetaDataService implements IMetaDataService {
     LocationModel? location;
     String? dpmVersion;
     BlockChain? blockChain;
-    Exif exif = await Exif.fromPath(filePath);
-    Map<String, Object>? data = await exif.getAttributes();
-    await exif.close();
-    if (data == null) {
+    final image = (await img.decodeJpgFile(filePath))!;
+    img.ExifData data = image.exif;
+    if (data.isEmpty) {
       throw "No Metadata found!";
     }
-    if (data.containsKey("Artist") || data.containsKey("AuxOwnerName")) {
-      if (!_isIOS!) {
-        secretHash = data["Artist"] as String;
-      } else {
-        secretHash = data["AuxOwnerName"] as String;
-      }
-    }
-    if (data.containsKey("GPSLatitude") && data.containsKey("GPSLongitude")) {
+    //TODO: Check for aritst tag
+    if (data.gpsIfd.hasGPSLatitude && data.gpsIfd.hasGPSLongitude) {
       location = LocationModel(
-          latitude: data["GPSLatitude"] as double,
-          longitude: data["GPSLongitude"] as double);
+          latitude: data.gpsIfd.gpsLatitude!,
+          longitude: data.gpsIfd.gpsLongitude!);
     }
-    if (data.containsKey("Software") || data.containsKey("FileSource")) {
-      if (!_isIOS!) {
-        dpmVersion = data["Software"] as String;
-        dpmVersion = dpmVersion.split(" ")[1];
-      } else {
-        dpmVersion = data["FileSource"] as String;
-        dpmVersion = dpmVersion.split(" ")[1];
-      }
+    if (data.imageIfd.hasSoftware) {
+      dpmVersion = data.imageIfd.software!;
+      dpmVersion = dpmVersion.split(" ")[1];
     } else {
-      throw "Invalid metadata!";
+      throw "No DPM Version found!";
     }
-    if (data.containsKey("UserComment")) {
-      blockChain = BlockChain.values
-          .firstWhere((chain) => chain.name == (data["UserComment"] as String));
+    if (data.imageIfd.hasUserComment) {
+      blockChain = BlockChain.values.firstWhere(
+          (chain) => chain.name == (data.imageIfd.userComment as String));
     } else {
-      throw "Invalid metadata!";
+      throw "No Blockchain found!";
     }
     return MetaDataModel(secretHash, location, dpmVersion, blockChain);
   }
