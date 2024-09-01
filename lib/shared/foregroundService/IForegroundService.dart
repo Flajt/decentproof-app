1f5ad2a587dbe316dbf53d8b64a4c90664a87dc2
Flajt <matrimkurz@gmail.com>
2024-09-01 19:06:39 +0200

    Chore: Refactor ForegroundServiceWrapper

--- lib/shared/foregroundService/IForegroundService.dart
+++ lib/shared/foregroundService/IForegroundService.dart
@@ -1,12 +1,12 @@
-import 'dart:isolate';
-
 abstract class IForegroundService {
+  Future<void> init();
   Future<void> start(
       Function? startCallback, String title, String descriptions);
   Future<void> stop();
   Future<T?> getData<T>(String key);
   Future<void> setData(String key, Object value);
   Future<void> updateNotification({String? title, String? body});
-  Future<ReceivePort> getReceivePort();
+  void registerOnReciveData(void Function(Object) callback);
+  void removeReciveDataCallback(void Function(Object) callback);
   Future<bool> get isRunning;
 }
