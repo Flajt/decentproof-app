58789c341acc051690599efcca913b64e5b3ff8f
Flajt <matrimkurz@gmail.com>
2024-09-19 18:54:25 +0200

    Fix: Add SizedBox to allow Column to use all space & center buttons

--- lib/features/verification/pages/VerificationPage.dart
+++ lib/features/verification/pages/VerificationPage.dart
@@ -1,9 +1,11 @@
 import 'package:decentproof/features/analytics/bloc/AnalyticsBloc.dart';
 import 'package:decentproof/features/analytics/bloc/AnalyticsEvents.dart';
 import 'package:decentproof/features/hashing/logic/backend/ShowInExplorer.dart';
+import 'package:decentproof/features/hashing/models/StatusModel.dart';
 import 'package:decentproof/features/verification/bloc/VerificationBloc.dart';
 import 'package:decentproof/features/verification/bloc/VerificationBlocEvents.dart';
 import 'package:decentproof/features/verification/bloc/VerificationBlocStates.dart';
+import 'package:decentproof/features/verification/models/VerificationStatusModel.dart';
 import 'package:decentproof/features/verification/uiblocks/BitcoinButton.dart';
 import 'package:decentproof/features/verification/uiblocks/CheckMarkTable.dart';
 import 'package:decentproof/features/verification/uiblocks/EthereumButton.dart';
@@ -24,76 +26,89 @@ class VerificationPage extends StatelessWidget {
     return Scaffold(
       // Note to self: With Foreground Task was here
       body: SafeArea(
-        child: BlocConsumer<VerificationBloc, VerificationBlocStates>(
-            listener: (context, state) {
-          if (state is ErrorState) {
-            showDialog(
-                context: context,
-                builder: (context) => ErrorDialog(
-                      size: size,
-                      error: state.message,
-                    ));
-            context.read<VerificationBloc>().add(ResetEvent());
-          }
-        }, builder: (context, state) {
-          if (state is InitialState) {
-            return Column(
-              children: [
-                Expanded(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: BlocConsumer<VerificationBloc, VerificationBlocStates>(
+              listener: (context, state) {
+            if (state is ErrorState) {
+              showDialog(
+                  context: context,
+                  builder: (context) => ErrorDialog(
+                        size: size,
+                        error: state.message,
+                      ));
+              context.read<VerificationBloc>().add(ResetEvent());
+            }
+          }, builder: (context, state) {
+            if (state is InitialState) {
+              return Column(
+                children: [
+                  Expanded(
+                      flex: 1,
+                      child: FractionallySizedBox(
+                        heightFactor: .5,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text("verificationPage.title",
+                                  style:
+                                      Theme.of(context).textTheme.headlineLarge)
+                              .tr(),
+                        ),
+                      )),
+                  Expanded(
                     flex: 1,
-                    child: FractionallySizedBox(
-                      heightFactor: .5,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text("verificationPage.title",
-                                style:
-                                    Theme.of(context).textTheme.headlineLarge)
-                            .tr(),
-                      ),
+                    child: Center(
+                        child: FilledButton(
+                      onPressed: () {
+                        context.read<AnalyticsBloc>().add(LogEvent(
+                            name: "verification_page_select_file",
+                            parameters: {}));
+                        context.read<VerificationBloc>().add(VerifyHashEvent());
+                      },
+                      child: const Text("verificationPage.selectFile").tr(),
                     )),
-                Expanded(
-                  flex: 1,
-                  child: Center(
-                      child: FilledButton(
-                    onPressed: () {
-                      context.read<AnalyticsBloc>().add(LogEvent(
-                          name: "verification_page_select_file",
-                          parameters: {}));
-                      context.read<VerificationBloc>().add(VerifyHashEvent());
-                    },
-                    child: const Text("verificationPage.selectFile").tr(),
-                  )),
-                ),
-              ],
-            );
-          } else if (state is VerifiedState) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text("verificationPage.title",
-                        style: Theme.of(context).textTheme.headlineLarge)
-                    .tr(),
-                CheckMarkTable(statusModel: state.statusModel),
-                state.statusModel.bitcoinTransaction != null
-                    ? BitcoinButton(
-                        showInExplorer: showInExplorer, state: state)
-                    : Container(),
-                state.statusModel.ethereumTransaction != null
-                    ? EthereumButton(
-                        showInExplorer: showInExplorer, state: state)
-                    : Container(),
-                OutlinedButton(
-                  onPressed: () =>
-                      context.read<VerificationBloc>().add(ResetEvent()),
-                  child: const Text("verificationPage.reset").tr(),
-                ),
-              ],
-            );
-            ;
-          }
-          return const Center(child: CircularProgressIndicator.adaptive());
-        }),
+                  ),
+                ],
+              );
+            } else if (state is VerifiedState) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("verificationPage.title",
+                          style: Theme.of(context).textTheme.headlineLarge)
+                      .tr(),
+                  CheckMarkTable(statusModel: state.statusModel),
+                  SizedBox(
+                    height: size.height * .2,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        state.statusModel.bitcoinTransaction != null
+                            ? BitcoinButton(
+                                showInExplorer: showInExplorer, state: state)
+                            : Container(),
+                        state.statusModel.ethereumTransaction != null
+                            ? EthereumButton(
+                                showInExplorer: showInExplorer, state: state)
+                            : Container(),
+                        OutlinedButton(
+                          onPressed: () => context
+                              .read<VerificationBloc>()
+                              .add(ResetEvent()),
+                          child: const Text("verificationPage.reset").tr(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            }
+            return const Center(child: CircularProgressIndicator.adaptive());
+          }),
+        ),
       ),
     );
   }
