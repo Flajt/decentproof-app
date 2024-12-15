91c35c400763de95e80f652456c3c1ec14ad3503
Flajt <matrimkurz@gmail.com>
2024-12-15 14:47:10 +0100

    Feat: Simply LocationTile logic

--- lib/features/metadata/uiblocks/LocationEmbeddingTile.dart
+++ lib/features/metadata/uiblocks/LocationEmbeddingTile.dart
@@ -5,15 +5,12 @@ import 'package:decentproof/features/settings/bloc/SettingsBlocStates.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
 
 class LocationEmbeddingTile extends StatelessWidget {
   const LocationEmbeddingTile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final service = GetIt.I.get<IMetaDataPermissionService>();
-
     return BlocBuilder<SettingsBloc, SettingsBlocStates>(
         builder: (context, state) {
       return ListTile(
@@ -21,9 +18,7 @@ class LocationEmbeddingTile extends StatelessWidget {
         subtitle: const Text("settingsPage.embedLocationSubtitle").tr(),
         trailing: Checkbox.adaptive(
             value: state is UpdatedSettingsState &&
-                    state.locationEmbeddingPermission
-                ? state.locationEmbeddingPermission
-                : service.shouldEmbedLocation(),
+                state.locationEmbeddingPermission,
             onChanged: (v) => context
                 .read<SettingsBloc>()
                 .add(ModifyLocationEmbeddingPermission(v ?? false))),
